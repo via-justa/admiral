dc6ac6fab63316d2e2b675228a2d548628fd76cf
via-justa <25720282+via-justa@users.noreply.github.com>
2021-01-03 10:42:23 +0100

    move ssh proxy inside mariadb

--- database/internal/mariadb/ssh.go
+++ database/internal/mariadb/ssh.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"fmt"
 	"net"
-
-	"github.com/via-justa/admiral/sshproxy"
+	"os"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/via-justa/admiral/config"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
 
 	"github.com/go-sql-driver/mysql"
 )
@@ -18,15 +18,49 @@ type viaSSHDialer struct {
 	client *ssh.Client
 }
 
+// Dial implement ssh dialer
 func (sshd *viaSSHDialer) Dial(addr string) (net.Conn, error) {
 	return sshd.client.Dial("tcp", addr)
+}
+
+func newSSHProxy(conf config.SSHProxy) (*ssh.Client, error) {
+	var agentClient agent.Agent
+
+	// Establish a connection to the local ssh-agent
+	if conn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		defer conn.Close() // nolint: errcheck
+
+		// Create a new instance of the ssh agent
+		agentClient = agent.NewClient(conn)
+	}
+
+	// The client configuration with configuration option to use the ssh-agent
+	sshConfig := &ssh.ClientConfig{
+		User:            conf.User,
+		Auth:            []ssh.AuthMethod{},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // nolint:gosec
+	}
+
+	// When the agentClient connection succeeded, add them as AuthMethod
+	if agentClient != nil {
+		sshConfig.Auth = append(sshConfig.Auth, ssh.PublicKeysCallback(agentClient.Signers))
+	}
+	// When there's a non empty password add the password AuthMethod
+	if conf.Password != "" {
+		sshConfig.Auth = append(sshConfig.Auth, ssh.PasswordCallback(func() (string, error) {
+			return conf.Password, nil
+		}))
+	}
+
+	// Connect to the SSH Server
+	return ssh.Dial("tcp", net.JoinHostPort(conf.Host, fmt.Sprint(conf.Port)), sshConfig)
 }
 
 // ProxyConnect starts new database connection via ssh proxy
 func ProxyConnect(conf *config.Config) (*Database, error) {
 	var db Database
 
-	sshcon, err := sshproxy.NewSSHProxy(conf.SSHProxy)
+	sshcon, err := newSSHProxy(conf.SSHProxy)
 	if err != nil {
 		return &db, err
 	}
